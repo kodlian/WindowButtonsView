eb68e06fd068e5971344f578d157d34017173fdf
jeremy marchand <contact@kodlian.com>
2014-09-16 19:45:29 +0200

    Initial Commit

---
+++ WindowButtonsView/WindowButtonsView.h
@@ -0,0 +1,19 @@
+//
+//  WindowButtonsView.h
+//  WindowButtonsView
+//
+//  Created by Jérémy Marchand on 16/09/2014.
+//  Copyright (c) 2014 Jérémy Marchand. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WindowButtonsView.
+FOUNDATION_EXPORT double WindowButtonsViewVersionNumber;
+
+//! Project version string for WindowButtonsView.
+FOUNDATION_EXPORT const unsigned char WindowButtonsViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WindowButtonsView/PublicHeader.h>
+
+
